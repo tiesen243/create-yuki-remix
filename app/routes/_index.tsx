a4e3bdf1fbeb59f2b97df702c4c744edb0c61922
tiesen243 <ttien56906@gmail.com>
2024-09-25 13:47:01 +0700

    feat(styles): enhance base styles and typography
    - Added relative, box-border, underline-offset-4, selection styles to all elements
    - Applied antialiased to body for better text rendering
    - Added text-balance to all heading tags (h1-h6)
    - Applied text-pretty to paragraph tags
    - Customized scrollbar styles with rounded corners and bg-ring color

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,13 @@
-import { Button } from '@/components/ui/button'
-
 const Page: React.FC = () => {
   return (
     <main className="container py-4">
-      lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, cumque.
-      <Button>Click me</Button>
+      <p className="text-4xl">
+        lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
     </main>
   )
 }
