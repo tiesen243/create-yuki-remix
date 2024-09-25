6370cf8623a8b6a8991bb0a0c165ec906e86a042
tiesen243 <ttien56906@gmail.com>
2024-09-25 13:39:59 +0700

    feat: update theme and layout components
    - Replaced `LinksFunction` with `MetaFunction` in `root.tsx`
    - Added `ThemeProvider` from `next-themes` for dark mode support
    - Simplified `Layout` component and updated its structure
    - Updated `Index` page to use a button component
    - Added custom CSS variables for theming in `tailwind.css`
    - Updated `tailwind.config.ts` to include new plugins and settings
    - Removed unused logo images from the public directory
    - Updated dependencies in `package.json` to include new packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,54 @@
 import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 export default {
+  darkMode: ['class'],
   content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: [
-          '"Inter"',
-          'ui-sans-serif',
-          'system-ui',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
+      fontFamily: { sans: ['Inter', ...fontFamily.sans] },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
       },
     },
   },
-  plugins: [],
+  plugins: [animate],
 } satisfies Config
